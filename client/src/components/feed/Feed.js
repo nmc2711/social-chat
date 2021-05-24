dffe0fa325d238f60b197aefb05d75d2b5717d13
hwangsanghan <nmc2711@station3.co.kr>
2021-05-24 13:26:17 +0900

    all push api

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios'
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import { Posts } from "../../dummyData";
+
 
 function Feed() {
+  const [post,setPosts] = useState([]);
+
+  useEffect(async() => {
+    const res = await axios.get("posts/timeline/60a9f99e3724d601307c42fa");
+    console.log(res)
+  }, [])
+
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
+        {/* {Posts.map((p) => (
           <Post key={p.id} post={p} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
